1a165e51cd77a7906136f810924708a8bb4800ca
Raz Zeevy <raz3zeevy@gmail.com>
2023-08-08 15:04:26 +0300

    update the predict.py after work in CLIP for new tokenizer

--- eval.sh
+++ eval.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 venv/bin/python eval_compo.py \
-  --coco_val_folder ./datasets/val2014/
-  --annotations_path datasets/annotations/captions_val2014.json \
-  --split_dataset_path data/dataset_splits/dataset_splits_1.json \
+  --coco-val-folder ./datasets/val2014/
+  --annotations-path datasets/annotations/captions_val2014.json \
+  --split-dataset-path data/dataset_splits/dataset_splits_1.json \
   --checkpoint ./checkpoint/test/test_check_g/coco_prefix-009.pt
+
